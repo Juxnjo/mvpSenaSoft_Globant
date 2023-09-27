d00b9f003f7040d1d94ae0303303b7b255113757
Juxnjo <ramirezjuanj0117@gmail.com>
2023-09-27 10:30:07 -0500

    front loginPage OK

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 
 import { Routes, Route } from 'react-router-dom'
 import { LoginPage } from './pages/LoginPage'
+import { RegisterPage } from "./pages/RegisterPage";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
       </Routes>
 
     </>
---
+++ frontend/src/components/LoginForm/LoginFormComponent.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+export const LoginFormComponent = () =>
+{
+    return (
+        <>
+            <form action="">
+                <input type="text" className='w-3/4 bg-white bg-opacity-20 placeholder:text-white border- border-gray-200   px-4 py-2 rounded-md my-2  ' placeholder="Username" required />
+
+                <input type="password" className='w-3/4 bg-white bg-opacity-20 placeholder:text-white border- border-gray-200   px-4 py-2 rounded-md my-2' placeholder="Password" required />
+
+                <button type='submit' className='bg-semiDarkBlue w-3/4 hover:bg-semiLightBlue text-white font-semibold my-2 py-2 px-4 rounded-md'>Login</button>
+            </form>
+            <div>
+                <p>Don't you have an account? <Link to='/register' className="text-lightBlue">Sign up.</Link></p>
+            </div>
+        </>
+
+
+
+
+
+
+    )
+}
