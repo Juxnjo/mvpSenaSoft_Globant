5833851c7a88d7aa8097a79deb0a5f7112d2c0d7
Juxnjo <ramirezjuanj0117@gmail.com>
2023-09-27 15:37:45 -0500

    dashboard frontend

---
+++ client/src/pages/DashboardPage.jsx
@@ -0,0 +1,13 @@
+import { SideBarComponent } from "../components/SideBar/SideBarComponent";
+import { Datatable } from "../components/TableDashboard/Datatable";
+
+export const DashboardPage = () =>
+{
+  return (
+    <div className="flex h-screen items-center justify-center text-center">
+      <SideBarComponent/>
+      <Datatable/>
+
+    </div>
+  )
+}
