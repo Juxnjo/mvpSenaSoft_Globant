ddb190b01abb81b7f48d28aa0ba6b3c2304453c9
millerbermeo <millerbermeo30@gmail.com>
2023-09-27 10:49:54 -0500

    create sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 
 import { Routes, Route } from 'react-router-dom'
 import { LoginPage } from './pages/LoginPage'
+import { HomePage } from './pages/HomePage'
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<LoginPage />} />
+        <Route path='/home' element={<HomePage />} />
       </Routes>
 
     </>
