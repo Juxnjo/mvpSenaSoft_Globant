89c7341bd3215c447c45604615c511f90cc8250b
millerbermeo <millerbermeo30@gmail.com>
2023-09-27 08:35:42 -0500

    config routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,15 @@
-<<<<<<< HEAD
+
 import { Routes, Route } from 'react-router-dom'
 import { LoginPage } from './pages/LoginPage'
-=======
-import { Routes, Route } from "react-router-dom"
 
->>>>>>> f59b89c2ef3251521fc5fec2b3d888d08e8152f1
-
-function App ()
-{
+function App() {
 
   return (
     <>
-<<<<<<< HEAD
-     <Routes>
-        <Route path='/' element={<LoginPage/>}/>
-     </Routes>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+      </Routes>
 
-=======
-     
-    <Routes>
-      {/* <Route path="/" element={<Login/>}/>
-      <Route path="/home" element={<Home/>}/> */}
-    </Routes>
->>>>>>> f59b89c2ef3251521fc5fec2b3d888d08e8152f1
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,20 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-<<<<<<< HEAD
+
 import { BrowserRouter } from 'react-router-dom'
-=======
-import { BrowserRouter } from "react-router-dom";
->>>>>>> f59b89c2ef3251521fc5fec2b3d888d08e8152f1
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-<<<<<<< HEAD
-    <App />
-=======
-      <App />
->>>>>>> f59b89c2ef3251521fc5fec2b3d888d08e8152f1
+      <App/>
     </BrowserRouter>
+
   </React.StrictMode>,
 )
