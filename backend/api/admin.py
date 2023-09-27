9aaff2c5cc819df6d5dfe9b51c3198bb0e0632e1
Daniel <dalobe56@gmail.com>
2023-09-27 11:03:04 -0500

    Subida de modelos de la BD

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Student, Areas, Tickets, FollowUp, Feedback
 
-# Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display= ('id', 'first_name', 'last_name', 'email')
+
+class AreasAdmin(admin.ModelAdmin):
+    list_display= ('id', 'name')
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display= ('id', 'student', 'area')
+
+class FollowUpAdmin(admin.ModelAdmin):
+    list_display= ('id', 'phase', 'ticket')
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display= ('id', 'phase', 'followUp')
+
+admin.site.register(Student, StudentAdmin)
+admin.site.register(Areas, AreasAdmin)
+admin.site.register(Tickets, TicketAdmin)
+admin.site.register(FollowUp, FollowUpAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
