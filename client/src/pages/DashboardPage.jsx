5f037f38603abcae713499b05dc3748696f7701f
Juxnjo <ramirezjuanj0117@gmail.com>
2023-09-27 16:56:47 -0500

    test

--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -5,7 +5,7 @@ export const DashboardPage = () => {
     <>
     <SideBarComponent/>
     <div className="h-screen py-14 sm:ml-0 pl-16 lg:pl-0 lg:ml-72 text-white flex gap-3 justify-between pr-10">
-        <h1 className="font-bold text-4xl">Hola! Usuario</h1>
+        <h1 className="font-bold text-4xl">Hola! Usuarioo</h1>
       </div>
       <div className="w-full flex justify-center">
         
