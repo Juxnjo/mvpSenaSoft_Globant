b0ffd7a828aa12a28780f0fef92a88ca2f548dfa
millerbermeo <millerbermeo30@gmail.com>
2023-09-27 18:29:17 -0500

    calendar create

--- client/src/components/Calendar/CalendarComponent.jsx
+++ client/src/components/Calendar/CalendarComponent.jsx
@@ -2,7 +2,59 @@ import React from 'react'
 
 export const CalendarComponent = () => {
   return (
-    <div>CalendarComponent</div>
+  <div className='bg-white bg-opacity-5 p-6 w-[500px]'>
+      <h1 className='mb-2 text-center font-bold text-2xl'>Enero 2023</h1>
+      <ol cla className='grid grid-cols-7 place-items-center text-right border-b-[1px] pb-2'>
+        <li class="day-name">Lun</li>
+        <li class="day-name">Mar</li>
+        <li class="day-name">Mié</li>
+        <li class="day-name">Jue</li>
+        <li class="day-name">Vie</li>
+        <li class="day-name">Sáb</li>
+        <li class="day-name">Dom</li>
+        <li class="first-day">1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>10</li>
+        <li>11</li>
+        <li>12</li>
+        <li>13</li>
+        <li>14</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>10</li>
+        <li>11</li>
+        <li>12</li>
+        <li>13</li>
+        <li>14</li>
+        <li>15</li>
+        <li>16</li>
+        <li>17</li>
+        <li>18</li>
+        <li>19</li>
+        <li>20</li>
+        <li>21</li>
+        <li>22</li>
+        <li>23</li>
+        <li>24</li>
+        <li>25</li>
+        <li>26</li>
+        <li>27</li>
+        <li>28</li>
+      </ol>
+    </div>
   )
 }
 
