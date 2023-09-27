d2be716007414c4f9955db79883fd0b67453b2e2
Juxnjo <ramirezjuanj0117@gmail.com>
2023-09-27 10:48:17 -0500

    frontend registerForm OK

--- frontend/src/components/RegisterForm/RegisterFormComponent.jsx
+++ frontend/src/components/RegisterForm/RegisterFormComponent.jsx
@@ -5,14 +5,16 @@ export const RegisterFormComponent = () =>
     return (
         <>
             <form action="">
-                <input type="text" className='w-3/4 bg-white bg-opacity-20 placeholder:text-white border- border-gray-200   px-4 py-2 rounded-md my-2  ' placeholder="Username" required />
+                <input type="email" className='w-3/4 bg-white bg-opacity-20 placeholder:text-white border- border-gray-200 px-4 py-2 rounded-md my-2  ' placeholder="Email" required />
 
-                <input type="password" className='w-3/4 bg-white bg-opacity-20 placeholder:text-white border- border-gray-200   px-4 py-2 rounded-md my-2' placeholder="Password" required />
+                <input type="text" className='w-3/4 bg-white bg-opacity-20 placeholder:text-white border- border-gray-200 px-4 py-2 rounded-md my-2  ' placeholder="Username" required />
 
-                <button type='submit' className='bg-semiDarkBlue w-3/4 hover:bg-semiLightBlue text-white font-semibold my-2 py-2 px-4 rounded-md'>Login</button>
+                <input type="password" className='w-3/4 bg-white bg-opacity-20 placeholder:text-white border- border-gray-200 px-4 py-2 rounded-md my-2' placeholder="Password" required />
+
+                <button type='submit' className='bg-semiDarkBlue w-3/4 hover:bg-semiLightBlue text-white font-semibold my-2 py-2 px-4 rounded-md'>Sign in</button>
             </form>
             <div>
-                <p>Don't you have an account? <Link to='/login' className="text-lightBlue">Sign up.</Link></p>
+                <p>Already have an account? <Link to='/' className="text-lightBlue">Log in.</Link></p>
             </div>
         </>
     )
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,13 +1,13 @@
-import {  } from "";
+import { RegisterFormComponent } from "../components/RegisterForm/RegisterFormComponent";
 
 export const RegisterPage = () => {
   return (
     <div className="flex h-[calc(100vh-100px)] items-center justify-center text-center">
       <div className="font-Roboto text-white">
         <h1 className="text-3xl font-semibold ">REGISTER ON </h1>
-        <h1 className="text-4xl text-semiLightBlue font-extrabold ">SAM</h1>
+        <h1 className="text-5xl text-semiLightBlue font-extrabold ">SAM</h1>
         <div className="">
-        
+          <RegisterFormComponent/>
         </div>
       </div>
       
